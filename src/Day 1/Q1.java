a75cf10d3fcf18a47abb9b969342b588dd73fbcb
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-10 19:56:58 +0530

    Hello World

--- src/Day 1/Q1.java
+++ src/Day 1/Q1.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Q1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if((n&1)==0){
+            System.out.println("ON");
+    }
+        else{
+            System.out.println("OFF");
+    }
+
     }
 }
