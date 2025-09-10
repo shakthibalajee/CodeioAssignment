4597d10ecf6143ceff593fd545e6e0f4bd765ef6
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-10 23:09:25 +0530

    Hello World

---
+++ src/Day 1/Q7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q7{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int op=sc.nextInt();
+        if(op==1){
+            System.out.println(a+b);
+        }
+        else if(op==2){
+            System.out.println(a-b);
+        }
+        else if(op==3){
+            System.out.println(a*b);
+        }
+        else if(op==4){
+            System.out.println(a/b);
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
