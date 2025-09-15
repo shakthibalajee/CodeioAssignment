7b94a93fc524b6768cfc07adfe9b62055de427d1
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-15 23:22:52 +0530

    Hello World

---
+++ src/Week 2/Seven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Seven{
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=n;
+        int sum=0;
+        while(x>0){
+            int rem=x%10;
+            sum+=(rem*rem*rem);
+            x/=10;
+        }
+        if(sum==n) System.out.println("Armstrong Number");
+        else System.out.println("Not Armstrong Number");
+    }
+}
