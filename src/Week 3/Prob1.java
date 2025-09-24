d82c54f6a9117c3b1b714d0d764945e1ab5f79f5
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-24 19:55:06 +0530

    Week 3 Assignment

--- src/Week 3/Prob1.java
+++ src/Week 3/Prob1.java
@@ -16,7 +16,10 @@ class Rectangle {
 }
 public class Prob1{
     public static void main(String[] args) {
-        Rectangle r1= new Rectangle(10,20);
+        Scanner sc= new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        Rectangle r1= new Rectangle(l,b);
         r1.area();
         r1.perimeter();
     }
