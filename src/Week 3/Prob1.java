83d817debcc57ae1e71b5d82b5b120a372d3c0cb
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-24 19:00:53 +0530

    Hello World

--- src/Week 3/Prob1.java
+++ src/Week 3/Prob1.java
@@ -1,20 +1,26 @@
 import java.util.*;
 class Rectangle {
-    void area(int length, int breadth) {
+    int length;
+    int breadth;
+    void area() {
         System.out.println("Area: "+ length * breadth);
     }
 
-    void perimeter(int length, int breadth) {
+    void perimeter() {
         System.out.println("Perimeter: "+ 2 * (length + breadth));
+    }
+    Rectangle(int length,int breadth){
+        this.length=length;
+        this.breadth=breadth;
     }
 }
 public class Prob1{
     public static void main(String[] args) {
-        Rectangle r1= new Rectangle();
-        Scanner sc= new Scanner(System.in);
-        int l=sc.nextInt();
-        int b=sc.nextInt();
-        r1.area(l,b);
-        r1.perimeter(l,b);
+        Rectangle r1= new Rectangle(10,20);
+//        Scanner sc= new Scanner(System.in);
+//        int l=sc.nextInt();
+//        int b=sc.nextInt();
+        r1.area();
+        r1.perimeter();
     }
 }
