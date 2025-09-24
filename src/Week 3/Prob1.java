a96a341874a209cb73ba87bb702b295acb6a3236
shakthibalajee <shakthibalajee28@gmail.com>
2025-09-24 19:01:41 +0530

    Hello World

--- src/Week 3/Prob1.java
+++ src/Week 3/Prob1.java
@@ -17,9 +17,6 @@ class Rectangle {
 public class Prob1{
     public static void main(String[] args) {
         Rectangle r1= new Rectangle(10,20);
-//        Scanner sc= new Scanner(System.in);
-//        int l=sc.nextInt();
-//        int b=sc.nextInt();
         r1.area();
         r1.perimeter();
     }
